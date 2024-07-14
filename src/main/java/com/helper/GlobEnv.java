d124c4ddb92cffb667751660ba5820e3c812c73e
RuslikMer <30290012+RuslikMer@users.noreply.github.com>
2024-07-14 17:08:47 +0400

    Update GlobEnv.java

--- src/main/java/com/helper/GlobEnv.java
+++ src/main/java/com/helper/GlobEnv.java
@@ -14,22 +14,22 @@ public class GlobEnv {
     public final static String NEW_LAST_NAME = "AutotesterNew";
 
     /** buyer's email used in tests */
-    public final static String BUYER_EMAIL = "b077164f183366d9e2f965ec27921108@bambinifashion.com";
+    public final static String BUYER_EMAIL = "@.com";
 
     /** buyer's password used in tests */
-    public final static String PASSWORD = "21452145";
+    public final static String PASSWORD = "";
 
     /** email for change */
-    public final static String CHANGE_EMAIL = "57478148313b29898934179694e45695@bambinifashion.com";
+    public final static String CHANGE_EMAIL = "@.com";
 
     /** new email */
-    public final static String NEW_EMAIL = "changedIosUser@bambinifashion.com";
+    public final static String NEW_EMAIL = "@.com";
 
     /** new password */
-    public final static String NEW_PASSWORD = "12345678";
+    public final static String NEW_PASSWORD = "";
 
     /** buyer's phone used in tests */
-    public final static String BUYER_PHONE = "+431521770";
+    public final static String BUYER_PHONE = "+";
 
     /** buyer's address used in tests */
     public final static String BUYER_ADDRESS = "Museumsplatz 1";
@@ -44,24 +44,24 @@ public class GlobEnv {
     public final static String BUYER_POSTCODE = "1070";
 
     /** buyer's stripes used in tests */
-    public final static String BUYER_STRIPES_NOT_AUTH = "4242 4242 4242 4242";
+    public final static String BUYER_STRIPES_NOT_AUTH = "";
 
-    public final static String BUYER_STRIPES_AUTH = "4000 0027 6000 3184";
+    public final static String BUYER_STRIPES_AUTH = "";
 
-    public final static String BUYER_STRIPES_PAUPER = "4000 0000 0000 9995";
+    public final static String BUYER_STRIPES_PAUPER = "";
 
     /** buyer's paypal login used in tests */
-    public final static String PAYPAL_LOGIN = "sb-uyqvi7126038@business.example.com";
+    public final static String PAYPAL_LOGIN = ".example.com";
 
     /** buyer's paypal password used in tests */
-    public final static String PAYPAL_PASS = "+yE#1D6@";
+    public final static String PAYPAL_PASS = "+@";
 
     /** buyer's paypal login used in tests */
-    public final static String SOFORT_CODE_GERMANY = "88888888";
+    public final static String SOFORT_CODE_GERMANY = "";
 
-    public final static String SOFORT_CODE_BELGIUM = "999";
+    public final static String SOFORT_CODE_BELGIUM = "";
 
-    public final static String SOFORT_CODE_OTHER = "00000";
+    public final static String SOFORT_CODE_OTHER = "";
 
     /** User for saved card test */
     public final static String USER_SAVED_CARD = "test-6@bambinifashion.com";
@@ -69,7 +69,7 @@ public class GlobEnv {
     /** User for change data test */
     public final static String USER_CHANGE = "test-2@bambinifashion.com";
 
-    public final static String IOS_DB_STAGE = "https://babydriver-ios.bambinimirror.com/";
+    public final static String IOS_DB_STAGE = "";
 
     public final static String DB_KEY = "ZHC3Zpubyd22btcGMru6CvyrFv2ov6";
 }
